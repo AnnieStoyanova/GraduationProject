36a6c7e8d8ae191d4f4b4858b45b6d3f2ef68e0a
AnnieStoyanova <ani100yanova777@gmail.com>
2024-01-31 11:30:29 +0200

    Add validations and remove unnecessary code

--- src/main/java/com/GraduationProject/graduation/config/SecurityConfig.java
+++ src/main/java/com/GraduationProject/graduation/config/SecurityConfig.java
@@ -8,11 +8,8 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,18 +20,6 @@ public class SecurityConfig {
 
     public SecurityConfig(UserService userService) {
         this.userService = userService;
-    }
-
-    @Bean
-    public InMemoryUserDetailsManager userDetailsInMemory() {
-        UserDetails user1 = User
-                .withUsername("SYSTEM")
-                .password("password1")
-                .build();
-        UserDetails user2 = User.withUsername("user2")
-                .password("password2")
-                .build();
-        return new InMemoryUserDetailsManager(user1, user2);
     }
 
 
@@ -54,15 +39,26 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
+
         http.authorizeHttpRequests
                         (authz -> authz
-                                // Преподавателите в системата могат да обработват данните в нея
-                                //.requestMatchers("/students/**").hasAuthority("TEACHER")
-                                //.requestMatchers("/students/**").hasAuthority("ADMIN")
-                                //.requestMatchers(HttpMethod.GET,"/students").hasAuthority("STUDENT")
-                                // Студентите качват единствено дипломната работа TODO:
-                                .anyRequest().authenticated()
-                                 //.anyRequest().permitAll()
+                                        // Преподавателите в системата могат да обработват данните в нея
+                                        //for the home page
+                                        .requestMatchers(HttpMethod.GET, "/").hasAnyAuthority("STUDENT", "ADMIN", "TEACHER")
+
+                                        // students can only see the other theses and can upload their
+                                        .requestMatchers(HttpMethod.GET, "/students/**").hasAnyAuthority("STUDENT", "ADMIN")
+                                        .requestMatchers("/applications/create-application").hasAnyAuthority("STUDENT", "ADMIN", "TEACHER")
+                                        .requestMatchers(HttpMethod.GET, "/applications").hasAnyAuthority("STUDENT", "ADMIN", "TEACHER")
+                                        .requestMatchers(HttpMethod.GET, "/theses").hasAnyAuthority("STUDENT", "ADMIN", "TEACHER")
+                                        .requestMatchers("/theses/create-thesis").hasAnyAuthority("STUDENT", "ADMIN", "TEACHER")
+                                        // students can see all approved applications
+                                        .requestMatchers("/applications/only-approved").hasAnyAuthority("STUDENT", "ADMIN", "TEACHER")
+                                        .requestMatchers("/**").hasAuthority("ADMIN")
+                                        .requestMatchers(HttpMethod.GET, "/**").hasAnyAuthority("TEACHER", "ADMIN")
+                                        //.requestMatchers("/resources/**").permitAll() // for the front end
+                                        .anyRequest().authenticated()
+                                //.anyRequest().permitAll()
                         )
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults())
--- src/main/java/com/GraduationProject/graduation/data/entity/Student.java
+++ src/main/java/com/GraduationProject/graduation/data/entity/Student.java
@@ -2,6 +2,7 @@ package com.GraduationProject.graduation.data.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -13,8 +14,11 @@ import lombok.Setter;
 @Table(name = "student")
 public class Student extends BaseEntity {
 
+    @NotBlank
     private String firstName;
+    @NotBlank
     private String lastName;
+    @NotBlank
     private String facultyNumber;
     private Integer graduationClass;
 }
